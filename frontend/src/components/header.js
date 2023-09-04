09e18a21b4977a6a0dbbc5c8ce80dc059fc1d135
Thahir <thahir@plgworks.com>
2023-09-04 12:32:56 +0530

    fix

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -6,8 +6,8 @@ export default function Header() {
     <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap z-60 text-sm py-[30px] w-full bg-white shadow-sm mb-[-1px]">
       <nav className="container w-full mx-auto sm:flex sm:items-center sm:justify-between px-[1.5rem]">
         <Link href="/" className={`font-bold text-[30px] text-[#27272a]`}>
-          <span className="text-[#ec4899]">T</span>
           {/* <span className="text-[#ec4899]">T</span>
+          <span className="text-[#ec4899]">T</span>
           <span className="text-[#ec4899]">r</span>
           <span className="text-[#ec4899]">a</span>
           <span className="text-[#ec4899]">v</span>
