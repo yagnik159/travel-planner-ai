69356ef06344c40d3a8fa0f5eb26e21b2a0edd86
Thahir <thahir@plgworks.com>
2023-09-04 12:32:30 +0530

    logo

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -7,19 +7,24 @@ export default function Header() {
       <nav className="container w-full mx-auto sm:flex sm:items-center sm:justify-between px-[1.5rem]">
         <Link href="/" className={`font-bold text-[30px] text-[#27272a]`}>
           <span className="text-[#ec4899]">T</span>
+          {/* <span className="text-[#ec4899]">T</span>
           <span className="text-[#ec4899]">r</span>
           <span className="text-[#ec4899]">a</span>
           <span className="text-[#ec4899]">v</span>
-          <span className="text-[#ec4899]">e</span>
-          <span className="text-[#ec4899]">l</span>
-          <span className="ml-[10px]"></span>
-          <span className="text-[#0369a1]">P</span>
-          <span className="text-[#0369a1]">l</span>
-          <span className="text-[#0369a1]">a</span>
+@ -19,7 +19,15 @@ export default function Header() {
           <span className="text-[#0369a1]">n</span>
           <span className="text-[#0369a1]">n</span>
           <span className="text-[#0369a1]">e</span>
           <span className="text-[#0369a1]">r</span>
+          <span className="text-[#0369a1]">r</span> */}
+
+          <span className="text-[#ec4899]">T</span>
+          <span className="text-[#ec4899]">r</span>
+          <span className="text-[#ec4899]">i</span>
+          <span className="text-[#ec4899]">p</span>
+          <span className="text-[#ec4899]">p</span>
+          <span className="text-[#0369a1]">I</span>
+          <span className="text-[#0369a1]">n</span>
         </Link>
       </nav>
     </header>
