7d9c3201d6909d77ab46b0f0bf5ef78252c48c95
Thahir <thahir@plgworks.com>
2023-09-04 14:41:23 +0530

    activities highlight

--- frontend/src/components/activities.js
+++ frontend/src/components/activities.js
@@ -18,119 +18,114 @@ export default function Activities() {
 
   return (
     <div className="grid gird-cols-3 gap-[30px]">
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <BeachesSvg />
-          <input
-            name="beaches"
-            type="checkbox"
-            value="beaches"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">Beaches</div>
-        </div>
+      <div className="relative p-[20px] w-full h-full text-left">
+        <BeachesSvg />
+        <input
+          name="beaches"
+          type="checkbox"
+          value="beaches"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Beaches
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <CitySvg />
-          <input
-            name="city"
-            type="checkbox"
-            value="city"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">
-            City sightseeing
-          </div>
-        </div>
+      <div className="relative p-[20px] w-full h-full text-left">
+        <CitySvg />
+        <input
+          name="city"
+          type="checkbox"
+          value="city"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          City sightseeing
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <HikingSvg />
-          <input
-            name="outdoor"
-            type="checkbox"
-            value="outdoor"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">
-            Outdoor adventures
-          </div>
-        </div>
+      <div className="relative p-[20px] w-full h-full text-left">
+        <HikingSvg />
+        <input
+          name="outdoor"
+          type="checkbox"
+          value="outdoor"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Outdoor adventures
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <FestivalSvg />
-          <input
-            name="events"
-            type="checkbox"
-            value="events"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">
-            Festivals/events
-          </div>
-        </div>
+
+      <div className="relative p-[20px] w-full h-full text-left">
+        <FestivalSvg />
+        <input
+          name="events"
+          type="checkbox"
+          value="events"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Festivals/events
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <RestaurantSvg />
-          <input
-            name="food"
-            type="checkbox"
-            value="food"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">
-            Food exploration
-          </div>
-        </div>
+
+      <div className="relative p-[20px] w-full h-full text-left">
+        <RestaurantSvg />
+        <input
+          name="food"
+          type="checkbox"
+          value="food"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Food exploration
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <NightLifeSvg />
-          <input
-            name="nightLife"
-            type="checkbox"
-            value="nightLife"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">Nightlife</div>
-        </div>
+
+      <div className="relative p-[20px] w-full h-full text-left">
+        <NightLifeSvg />
+        <input
+          name="nightLife"
+          type="checkbox"
+          value="nightLife"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Nightlife
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <ShoppingSvg />
-          <input
-            name="shopping"
-            type="checkbox"
-            value="shopping"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">Shopping</div>
-        </div>
+
+      <div className="relative p-[20px] w-full h-full text-left">
+        <ShoppingSvg />
+        <input
+          name="shopping"
+          type="checkbox"
+          value="shopping"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Shopping
+        </label>
       </div>
-      <div className="border">
-        <div className="relative p-[20px] w-full h-full text-left">
-          <SpaSvg />
-          <input
-            name="spa"
-            type="checkbox"
-            value="spa"
-            onChange={onChange}
-            className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0"
-          />
-          <div className="mt-2 text-sm sm:text-base font-medium">
-            Spa wellness
-          </div>
-        </div>
+
+      <div className="relative p-[20px] w-full h-full text-left">
+        <SpaSvg />
+        <input
+          name="spa"
+          type="checkbox"
+          value="spa"
+          onChange={onChange}
+          className="absolute w-full h-full left-0 right-0 top-0 bottom-0 opacity-0 cursor-pointer"
+        />
+        <label className="mt-2 text-sm sm:text-base font-medium fullContainer">
+          Spa wellness
+        </label>
       </div>
     </div>
   );
