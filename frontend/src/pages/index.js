6b4c2b11d0234376b4b455048eb3f3e2f6f33f84
Thahir <thahir@plgworks.com>
2023-09-05 15:07:07 +0530

    cta

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
           </p>
           <Link
             href="/plan"
-            className="bg-[#000] text-[#fff] border-2 text-[18px] font-medium px-[2rem] py-[1rem] inline-block hover:bg-transparent hover:border-black hover:text-[#000] transition tracking-wider"
+            className={`bg-[#000] text-[#fff] border-2 text-[18px] font-medium px-[2rem] py-[1rem] inline-block hover:bg-transparent transition tracking-wider ${styles.heroCta}`}
           >
             Plan Now to Travel
           </Link>
